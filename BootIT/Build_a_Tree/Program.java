c948e0b86b40c482dfb595e2aae620637ac71fe7
Jiali Zhang <zhangjiali86@gmail.com>
2023-08-18 23:01:57 +0200

    tree

--- BootIT/Build_a_Tree/Program.java
+++ BootIT/Build_a_Tree/Program.java
@@ -1,5 +1,12 @@
 package Build_a_Tree;
 
 public class Program {
+    public static void main(String[] args) {
+        System.out.println(".........");
+        Tree Jiali = new Tree(1, 1, 1, 6);
+        Jiali.display();
+        Jiali.grow();
+        Jiali.display();
+    }
 
 }
--- BootIT/Build_a_Tree/Tree.java
+++ BootIT/Build_a_Tree/Tree.java
@@ -1,5 +1,32 @@
 package Build_a_Tree;
 
 public class Tree {
+    double height;
+    int age;
+    double growth_rate;
+    int max_age;
+
+    Tree(double h, int a, double r, int m_a) {
+        height = h;
+        age = a;
+        growth_rate = r;
+        max_age = m_a;
+    }
+
+    void display() {
+        if (age % 5 > 0) {
+            System.out.println("Your tree is currently" + age + "years old.");
+            System.out.println("It has reached the height of" + height + " cm.");
+        } else
+            System.out.println("Your tree is currently" + age + "years old.");
+
+    }
+
+    void grow() {
+        while (age < max_age && age % 5 != 0) {
+            age += 1;
+            System.out.println("And your tree just grew a year older!");
+        }
+    }
 
 }
