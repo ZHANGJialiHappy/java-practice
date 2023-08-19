c098db107746128143db16e91c0717d4ac4314ec
Jiali Zhang <zhangjiali86@gmail.com>
2023-08-19 10:09:25 +0200

    Tree

--- BootIT/Build_a_Tree/Program.java
+++ BootIT/Build_a_Tree/Program.java
@@ -3,10 +3,15 @@ package Build_a_Tree;
 public class Program {
     public static void main(String[] args) {
         System.out.println(".........");
-        Tree Jiali = new Tree(1, 1, 1, 6);
+        Tree Jiali = new Tree(1, 1, 1, 101);
         Jiali.display();
+        System.out.println(".........");
         Jiali.grow();
+        System.out.println(".........");
         Jiali.display();
+        // Jiali.grow();
     }
 
 }
+
+// out put is right, but method is different.
