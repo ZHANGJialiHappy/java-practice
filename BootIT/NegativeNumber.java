196428bd0bc4ef8c4dc6801d2283f65ea05511c8
Jiali Zhang <zhangjiali86@gmail.com>
2023-08-30 14:03:58 +0200

    delete files

--- BootIT/NegativeNumber.java
+++
@@ -1,7 +0,0 @@
-public class NegativeNumber {
-    public static void main(String[] args) {
-        System.out.println(-273.15);
-        System.out.println(-273.15);
-        System.out.println(-273.15);
-    }
-}
--- BootIT/number.java
+++
@@ -1,5 +0,0 @@
-public class number {
-    public static void main(String[] args) {
-        System.out.println("2023");
-    }
-}
