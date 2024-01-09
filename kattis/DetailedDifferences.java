aaa856169d0dd60964e720871b0aa24db5e42500
Jiali Zhang <zhangjiali86@gmail.com>
2024-01-09 10:35:36 +0100

    update Skener and DetailedDifferences

--- kattis/DetailedDifferences.java
+++ kattis/DetailedDifferences.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DetailedDifferences {
@@ -9,30 +11,34 @@ public class DetailedDifferences {
             sc.close();
             throw new IllegalArgumentException("the number of test should between 1 and 500");
         }
-        Scanner sc2 = new Scanner(System.in);
-        int count = 0;
-        while (count < num && sc2.hasNextLine()) {
-            String str1 = sc2.nextLine();
-            String str2 = sc2.nextLine();
+        List<String> str1s = new ArrayList<>();
+        List<String> str2s = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+            String str1 = sc.next();
+            String str2 = sc.next();
+            str1s.add(str1);
+            str2s.add(str2);
+        }
+
+        for (int i = 0; i < str1s.size(); i++) {
+            String str1 = str1s.get(i);
+            String str2 = str2s.get(i);
+
             System.out.println(str1);
             System.out.println(str2);
-
             for (int j = 0; j < str1.length(); j++) {
                 if (str1.charAt(j) == str2.charAt(j)) {
                     System.out.print(".");
                 } else {
                     System.out.print("*");
                 }
-
             }
             System.out.println();
             System.out.println();
 
-            count++;
         }
         sc.close();
-        sc2.close();
-
     }
 
 }
---
+++ kattis/Skener.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Skener {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int zr = sc.nextInt();
+        int zc = sc.nextInt();
+        String[][] lines = new String[r][c];
+        for (int i = 0; i < r; i++) {
+            String row = sc.next();
+            for (int j = 0; j < c; j++) {
+                lines[i][j] = row.substring(j, j + 1);
+            }
+        }
+
+        for (String[] row : lines) {
+            for (int j = 0; j < zr; j++) {
+                for (String column : row) {
+                    for (int i = 0; i < zc; i++) {
+                        System.out.print(column);
+                    }
+                }
+                System.out.println();
+
+            }
+
+        }
+    }
+}
