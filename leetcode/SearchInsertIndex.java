f854782171693db627abcbd79a4b45143c36ee8d
Jiali Zhang <zhangjiali86@gmail.com>
2024-01-16 13:29:23 +0100

    leetcode: update searchInsertPosition

--- leetcode/SearchInsertIndex.java
+++ leetcode/SearchInsertIndex.java
@@ -1,4 +1,4 @@
-public class SearchInsertPosition {
+public class SearchInsertIndex {
     public int searchInsert(int[] nums, int target) {
         if (target > nums[nums.length - 1]) {
             return nums.length;
@@ -19,10 +19,4 @@ public class SearchInsertPosition {
         }
         return 0;
     }
-
-    public static void main(String[] args) {
-        SearchInsertPosition a = new SearchInsertPosition();
-        System.out.println(a.searchInsert(new int[] { 1, 3, 5, 6 }, 6));
-    }
-
 }
