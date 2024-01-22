65de1aa9296fcaeec2145c60785df2dc4f8982d8
Jiali Zhang <zhangjiali86@gmail.com>
2024-01-22 15:10:39 +0100

    leetcode: climbingstair

---
+++ leetcode/ClimbingStairs.java
@@ -0,0 +1,20 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs a = new ClimbingStairs();
+        System.out.println(a.climbStairs(2));
+    }
+
+}
